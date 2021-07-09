739a3acfb47a7cc9640af7c36b24143a6a35f3ba
Benício Barreto <benicio.daniel@gmail.com>
2021-07-09 18:23:52 -0300

    creating Home

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,45 @@
 <template>
- <h1 class="text-brand-main">Aqui é a home</h1>
+ <custom-header
+  @create-account="handleCreateAccount"
+  @login="handleLogin"
+ />
+ <contact/>
+ <div class="flex justify-center py-10 bg-brand-gray">
+   <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+ </div>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from '@/views/Home/CustomHeader'
+import Contact from '@/views/Home/Contact'
 
+export default {
+  components: {
+    'custom-header': CustomHeader,
+    contact: Contact
+  },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+    }
+
+    function handleCreateAccount () {
+    }
+
+    return {
+      handleLogin,
+      handleCreateAccount
+    }
+  }
 }
 </script>
