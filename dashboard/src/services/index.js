f88467c204a4d6a68fa7b39482f85802be66f506
Benício Barreto <benicio.daniel@gmail.com>
2021-07-24 20:05:29 -0300

    added global state

--- dashboard/src/services/index.js
+++ dashboard/src/services/index.js
@@ -1,6 +1,9 @@
 import AuthService from './auth'
+import UsersService from './users'
+
 import axios from 'axios'
 import router from '../router'
+import { setGlobalLoading } from '../store/global'
 
 const API_ENVS = {
   production: '',
@@ -12,6 +15,7 @@ const httpClient = axios.create({
 })
 
 httpClient.interceptors.request.use(config => {
+  setGlobalLoading(true)
   const token = window.localStorage.getItem('token')
   if (token) {
     config.headers.common.Authorization = `Bearer ${token}`
@@ -19,12 +23,16 @@ httpClient.interceptors.request.use(config => {
   return config
 })
 
-httpClient.interceptors.response.use((response) => response, (error) => {
+httpClient.interceptors.response.use((response) => {
+  setGlobalLoading(false)
+  return response
+}, (error) => {
   const canThrowAnError = error.request.status === 0 || error.request.status === 500
 
   console.log('canThrowAnError', canThrowAnError)
 
   if (canThrowAnError) {
+    setGlobalLoading(false)
     throw new Error(error.message)
   }
 
@@ -32,9 +40,11 @@ httpClient.interceptors.response.use((response) => response, (error) => {
     router.push({ name: 'Home' })
   }
 
+  setGlobalLoading(false)
   return error
 })
 
 export default {
-  auth: AuthService(httpClient)
+  auth: AuthService(httpClient),
+  users: UsersService(httpClient)
 }
